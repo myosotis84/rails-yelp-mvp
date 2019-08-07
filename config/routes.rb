85f278dfb5d866b884db579660f6ad3a0749be66
Emma Laurent <emma.laurent.je@gmail.com>
2019-08-07 16:04:34 +0200

    added controllers, views and style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # get 'restaurants', to: 'restaurant#index', as: :restaurants
+  # post 'restaurants', to: 'restaurant#create'
+  # get 'restaurants/new', to: 'restaurant#new', as: :new_restaurant
+  # get 'restaurants/:id', to: 'restaurant#show', as: :restaurant
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # member do
+
+  # end
 end
